42691b934b31770c86a6cede02e5031041c24725
DovidM <landyx47@gmail.com>
2017-06-20 22:06:02 -0400

    Added support for changing notification, 2fa, and email preferences

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,13 +1,39 @@
 const router = require('express').Router();
 const User = require("../../controller/classes/User");
+const Utilities = require('../../controller/classes/Utilities');
+
+const UserInstance = new User();
+
 
 router.get("/", async function(req, res) {
 
-    const UserInstance = new User();
-
     await UserInstance.defineInfoFor(req.query.user);
-
     res.send(await UserInstance.getAllUserInfo());
+});
+
+router.put('/', async function(req, res) {
+
+    await UserInstance.defineInfoFor(UserInstance.getJWT().id, true);
+
+    if (await UserInstance.checkPassword(req.body.pass)) {
+
+        if (req.body['2fa'] !== undefined) {
+            UserInstance.setTwoFactor(req.body['2fa']);
+        }
+
+        if (req.body['notifications'] !== undefined) {
+            UserInstance.setNotificationStatus(req.body.notifications);
+        }
+
+        if (req.body.userEmail) {
+            UserInstance.setEmail(req.body.userEmail);
+        }
+console.log('sdfs');
+        UserInstance.destruct();
+
+        Utilities.setHeader(200, 'user(s) updated');
+    }
+
 });
 
 module.exports = router
