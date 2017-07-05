deb3ab11a9f36254740e75195c064e806e355b3f
DovidM <landyx47@gmail.com>
2017-07-05 16:48:48 -0400

    Abstracted away creating a table

---
+++ client/src/components/Table.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+/**
+ * @prop headings - array
+ * @prop rows - 2d array where [row][tdText]
+ */
+function Table(props) {
+    console.log(props.rows)
+
+    return (<table>
+        <thead>
+            <tr>
+                {props.headings.map((text, idx) => <th key={idx}>{text}</th>)}
+            </tr>
+        </thead>
+        <tbody>
+            {props.rows.map((row, idx) =>
+                <tr key={idx}>
+                    {row.map((cellText, idx) => <td key={idx}>{cellText}</td>)}
+                </tr>)}
+        </tbody>
+    </table>);
+}
+
+export default Table;
