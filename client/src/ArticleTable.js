699ffc314d2ec2d8558ba3e03a2ba643ac937eed
DovidM <landyx47@gmail.com>
2017-07-08 22:04:11 -0400

    Worked on getting latest article info to show correctly

--- client/src/ArticleTable.js
+++ client/src/ArticleTable.js
@@ -19,47 +19,64 @@ class ArticleTable extends React.Component {
         };
     }
 
-    async componentWillMount() {
-
+    async getData() {
         // NOTE: REMOVE query after done building this
-        const rawData= await fetch('/api/articleGroup?articlesFor=1', {
+         return await fetch('/api/articleGroup?articlesFor=1', {
             credentials: "include",
             headers: {
                 "Content-Type": "application/json"
             }
         });
 
-        const data = await rawData.json();
+    }
 
-        const articleArr = data[0].map(article => {
+    async componentWillMount() {
 
-            const tagArr = article.tags.split(', ');
+        const data = await this.getData();
+        const json = await data.json();
+        this.setArticleInfo(json);
+    }
 
-            return [
-                <a href={`/issue/${data[1].num}/story/${article.url}`}>{decodeURIComponent(article.url)}</a>,
-                article.created,
-                <a href={`/u/${article.author_username}`}>{article.author_name}</a>,
-                <select name="tag[]" formMethod="put" defaultValue={tagArr} required multiple>{data[1].map((val) =>
-                     <option key={val} value={val}>{val}</option>
-                )}</select>,
-                article.views,
-                <input type="number" formMethod="put" name="order[]" value={article.display_order} />,
-                <SecretTwins
-                  original={<input type="checkbox" formMethod="delete" name="delArt[]" value={article.art_id} />}
-                  props = {{
-                      name: "artId[]"
-                  }}
+    setArticleInfo(data) {
 
-                />
-            ]
+        const articles = data[0].map(article => {
+
+                const tagArr = article.tags.split(', ');
+
+                return [
+                    <a href={`/issue/${data[1].num}/story/${article.url}`}>{decodeURIComponent(article.url)}</a>,
+                    article.created,
+                    <a href={`/u/${article.author_username}`}>{article.author_name}</a>,
+                    <select name="tag[]" formMethod="put" defaultValue={tagArr} required multiple>{data[1].map((val) =>
+                        <option key={val} value={val}>{val}</option>
+                    )}</select>,
+                    article.views,
+                    <input type="number" formMethod="put" name="order[]" value={article.display_order} />,
+                    <SecretTwins
+                    original={<input type="checkbox" formMethod="delete" name="delArt[]" value={article.art_id} />}
+                    props = {{
+                        name: "artId[]",
+                        formMethod: "put"
+                    }}
+
+                    />
+                ]
         });
 
-        this.setState({articles: articleArr});
+        this.setState({articles, issueInfo: data[2]});
     }
 
     render() {
 
-        const headings = ['Article', 'Date Created', 'Uploaded By', 'Type', 'Views', 'Display Order', <span className="danger">Delete</span>];
+        const headings = [
+            'Article',
+            'Date Created',
+            'Uploaded By',
+            'Type',
+            'Views',
+            'Display Order',
+            <span className="danger">Delete</span>
+        ];
 
         return (
             <Container
@@ -67,7 +84,22 @@ class ArticleTable extends React.Component {
                 className="tableContainer"
                 children={<Form
                                method={['put', 'delete']}
-                              children={<Table headings={headings} rows={this.state.articles}/>}
+                              children={
+                                  <div>
+                                    <Table headings={headings} rows={this.state.articles}/>
+
+                                    <Input
+                                      label="Issue Name"
+                                      props={{
+                                          name: "issueName",
+                                          defaultValue: this.state.issueInfo.name,
+                                          key: this.state.issueInfo.name, // makes it update when fetch info comes
+                                          disabled: !!this.state.issueInfo.ispublic,
+                                          type: "text"
+                                      }}
+                                    />
+                                  </div>
+                          }
                           />}
             />
         )
