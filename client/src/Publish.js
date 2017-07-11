dc79d1d5464e775bf010cfa471933887e16825e8
DovidM <landyx47@gmail.com>
2017-07-11 12:47:50 -0400

    Fixed bug where tinymce.css couldn't load when coming from url with multiple 'directories'

--- client/src/Publish.js
+++ client/src/Publish.js
@@ -39,7 +39,7 @@ class Publish extends React.Component {
           plugins: 'wordcount table fullscreen autolink autoresize help image paste code',
           menubar: "view help edit tools",
           valid_elements: "abbr[!title],h1,h2,h3,h4,h5,h6,img[!src|alt],p,a[!href],table,td,tr,th,tbody,thead,tfoot,strong,em,u,ul,ol,li,q,blockquote,pre,br",
-          content_css: "./tinymce.css",
+          content_css: "/tinymce.css",
           paste_data_images: true,
           setup: editor => {
 
