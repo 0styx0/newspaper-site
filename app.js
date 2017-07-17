022cbe715409ee66f8f5355914bc213596604a0c
DovidM <landyx47@gmail.com>
2017-07-16 22:25:30 -0400

    Different cors origin

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3002");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
+  //res.header("Access-Control-Allow-Origin", "http://localhost:5000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Credentials", true);
   next();
