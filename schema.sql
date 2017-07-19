dfd263952c37f38f9ce67a444ae3ac792363039d
DovidM <landyx47@gmail.com>
2017-07-18 20:50:58 -0400

    Uncapitalized field names

--- schema.sql
+++ schema.sql
@@ -17,18 +17,18 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `COMMENTS`
+-- Table structure for table `comments`
 --
 
-CREATE TABLE IF NOT EXISTS `COMMENTS` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `ART_ID` int(11) NOT NULL,
-  `AUTHORID` int(11) NOT NULL,
-  `CONTENT` varchar(500) DEFAULT NULL,
-  `CREATED` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`ID`),
-  KEY `ART_ID` (`ART_ID`),
-  KEY `AUTHORID` (`AUTHORID`)
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `art_id` int(11) NOT NULL,
+  `authorid` int(11) NOT NULL,
+  `content` varchar(500) DEFAULT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `art_id` (`art_id`),
+  KEY `authorid` (`authorid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=360 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -38,75 +38,75 @@ CREATE TABLE IF NOT EXISTS `COMMENTS` (
 --
 
 CREATE TABLE IF NOT EXISTS `issues` (
-  `NUM` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `ISPUBLIC` tinyint(1) NOT NULL DEFAULT '0',
-  `NAME` varchar(20) DEFAULT NULL,
-  `MADEPUB` date DEFAULT NULL,
-  PRIMARY KEY (`NUM`),
-  UNIQUE KEY `NAME` (`NAME`)
+  `num` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ispublic` tinyint(1) NOT NULL DEFAULT '0',
+  `name` varchar(20) DEFAULT NULL,
+  `madepub` date DEFAULT NULL,
+  PRIMARY KEY (`num`),
+  UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `PAGEINFO`
+-- Table structure for table `pageinfo`
 --
 
-CREATE TABLE IF NOT EXISTS `PAGEINFO` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `CREATED` date DEFAULT NULL,
-  `URL` varchar(75) NOT NULL,
-  `LEDE` blob NOT NULL,
-  `IMG_URL` blob,
-  `SLIDE_IMG` varchar(100) DEFAULT NULL,
-  `BODY` blob NOT NULL,
-  `ISSUE` int(11) NOT NULL,
-  `AUTHORID` varchar(8) DEFAULT NULL,
-  `VIEWS` int(11) NOT NULL DEFAULT '0',
-  `DISPLAY_ORDER` int(2) UNSIGNED NOT NULL DEFAULT '0',
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `URL` (`URL`),
-  UNIQUE KEY `ISSUE` (`ISSUE`,`URL`)
+CREATE TABLE IF NOT EXISTS `pageinfo` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `created` date DEFAULT NULL,
+  `url` varchar(75) NOT NULL,
+  `lede` blob NOT NULL,
+  `img_url` blob,
+  `slide_img` varchar(100) DEFAULT NULL,
+  `body` blob NOT NULL,
+  `issue` int(11) NOT NULL,
+  `authorid` varchar(8) DEFAULT NULL,
+  `views` int(11) NOT NULL DEFAULT '0',
+  `display_order` int(2) UNSIGNED NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `url` (`url`),
+  UNIQUE KEY `issue` (`issue`,`url`)
 ) ENGINE=InnoDB AUTO_INCREMENT=25855 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `TAGS`
+-- Table structure for table `tags`
 --
 
-CREATE TABLE IF NOT EXISTS `TAGS` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `ART_ID` int(11) NOT NULL,
-  `TAG1` varchar(15) NOT NULL,
-  `TAG2` varchar(15) DEFAULT NULL,
-  `TAG3` varchar(15) DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `ART_ID` (`ART_ID`)
+CREATE TABLE IF NOT EXISTS `tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `art_id` int(11) NOT NULL,
+  `tag1` varchar(15) NOT NULL,
+  `tag2` varchar(15) DEFAULT NULL,
+  `tag3` varchar(15) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `art_id` (`art_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=24934 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `USERS`
+-- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `USERS` (
-  `ID` int(11) NOT NULL AUTO_INCREMENT,
-  `USERNAME` char(20) NOT NULL,
-  `F_NAME` varchar(10) NOT NULL,
-  `M_NAME` char(3) DEFAULT 'N/A',
-  `L_NAME` varchar(20) NOT NULL,
-  `PASSWORD` text NOT NULL,
-  `EMAIL` varchar(255) DEFAULT NULL,
-  `LEVEL` int(11) UNSIGNED NOT NULL,
-  `AUTH` text,
-  `AUTH_TIME` timestamp NULL DEFAULT NULL,
-  `NOTIFICATIONS` tinyint(1) NOT NULL DEFAULT '1',
-  `TWO_FA_ENABLED` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`ID`),
-  UNIQUE KEY `USERNAME` (`USERNAME`),
-  UNIQUE KEY `F_NAME` (`F_NAME`,`M_NAME`,`L_NAME`)
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` char(20) NOT NULL,
+  `f_name` varchar(10) NOT NULL,
+  `m_name` char(3) DEFAULT 'N/A',
+  `l_name` varchar(20) NOT NULL,
+  `password` text NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `level` int(11) UNSIGNED NOT NULL,
+  `auth` text,
+  `auth_time` timestamp NULL DEFAULT NULL,
+  `notifications` tinyint(1) NOT NULL DEFAULT '1',
+  `two_fa_enabled` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `f_name` (`f_name`,`m_name`,`l_name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=15586 DEFAULT CHARSET=utf8;
 
 --
@@ -114,14 +114,14 @@ CREATE TABLE IF NOT EXISTS `USERS` (
 --
 
 --
--- Constraints for table `COMMENTS`
+-- Constraints for table `comments`
 --
-ALTER TABLE `COMMENTS`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`ART_ID`) REFERENCES `PAGEINFO` (`ID`),
-  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`AUTHORID`) REFERENCES `USERS` (`ID`);
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`art_id`) REFERENCES `pageinfo` (`id`),
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`authorid`) REFERENCES `users` (`id`);
 
 --
--- Constraints for table `TAGS`
+-- Constraints for table `tags`
 --
-ALTER TABLE `TAGS`
-  ADD CONSTRAINT `tags_ibfk_1` FOREIGN KEY (`ART_ID`) REFERENCES `PAGEINFO` (`ID`);
+ALTER TABLE `tags`
+  ADD CONSTRAINT `tags_ibfk_1` FOREIGN KEY (`art_id`) REFERENCES `pageinfo` (`id`);
