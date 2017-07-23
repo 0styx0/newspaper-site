f5a0bbbfc9a7548d7cee6e1edc7ac6aa812ed5b5
DovidM <landyx47@gmail.com>
2017-07-23 08:46:33 -0400

    Simplified Logout

--- client/src/components/Logout/index.js
+++ client/src/components/Logout/index.js
@@ -2,50 +2,25 @@ import React from 'react';
 import FormContainer from '../Form/container';
 import {jwt} from '../jwt';
 
-import { Redirect } from 'react-router'
+import { Link } from 'react-router-dom'
 
+function Logout(props) {
 
-class Logout extends React.Component {
-
-    constructor() {
-        super();
-
-        this.onLogout = this.onLogout.bind(this);
-
-        this.state = {
-            redirect: false
-        }
-    }
-
-    onLogout() {
-
-        jwt.level = 0;
-
-        this.setState({
-            redirect: true
-        })
-    }
-
-    render() {
-
-        return (
-            <span>
-                <FormContainer
-                    method="put"
-                    action="/api/userStatus"
-                    onSubmit={this.onLogout}
-                    children={<input
-                                className="changed"
-                                id="logoutInpt"
-                                type="submit"
-                                name="logout"
-                                value="Log Out"
-                               />}
-                />
-                {this.state.redirect ? <Redirect to="/" /> : ""}
-            </span>
-        )
-    }
+    return (
+        <Link to="/" onClick={() => jwt.level = 0}>
+            <FormContainer
+                method="put"
+                action="/api/userStatus"
+                children={<input
+                            className="changed"
+                            id="logoutInpt"
+                            type="submit"
+                            name="logout"
+                            value="Log Out"
+                            />}
+            />
+        </Link>
+    )
 }
 
 export default Logout;
