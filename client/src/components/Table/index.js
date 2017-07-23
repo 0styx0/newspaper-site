2e405d2d0c50faa1df9709c76ea293106e4dc481
DovidM <landyx47@gmail.com>
2017-07-23 15:29:10 -0400

    Added PropTypes

--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './index.css';
 
@@ -6,24 +7,34 @@ import './index.css';
  * @prop headings - array
  * @prop rows - 2d array where [row][tdText]
  */
-class Table extends React.Component {
+function Table(props) {
 
-    render() {
+    return (<table>
+        <thead>
+            <tr>
+                {props.headings.map((text, idx) => <th key={idx}> {text && text[0] ? text[0].toUpperCase() + text.slice(1) : text}</th>)}
+            </tr>
+        </thead>
+        <tbody>
+            {props.rows.map((row, idx) =>
+                <tr key={idx}>
+                    {row.map((cellText, idx) => <td key={idx}>{cellText}</td>)}
+                </tr>)}
+        </tbody>
+    </table>);
+}
 
-        return (<table>
-            <thead>
-                <tr>
-                    {this.props.headings.map((text, idx) => <th key={idx}> {text && text[0] ? text[0].toUpperCase() + text.slice(1) : text}</th>)}
-                </tr>
-            </thead>
-            <tbody>
-                {this.props.rows.map((row, idx) =>
-                    <tr key={idx}>
-                        {row.map((cellText, idx) => <td key={idx}>{cellText}</td>)}
-                    </tr>)}
-            </tbody>
-        </table>);
-    }
+Table.propTypes = {
+    headings: PropTypes.arrayOf(PropTypes.oneOfType([
+                                    PropTypes.string,
+                                    PropTypes.element
+                                ])
+                                ).isRequired, // th
+    rows: PropTypes.arrayOf( // tr
+        PropTypes.arrayOf(
+                PropTypes.any
+            )
+        ).isRequired
 }
 
 export default Table;
