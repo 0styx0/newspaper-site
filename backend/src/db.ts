ce38062ac469ae02e7bc38ce1239703c3fafb625
DovidM <landyx47@gmail.com>
2017-08-07 21:38:24 -0400

    Added sequelize connection

---
+++ backend/src/db.ts
@@ -0,0 +1,19 @@
+const Sequelize = require('sequelize'); // typescript throws errors if do es6 import
+const { DB } = require('../../../config.json');
+
+const sequelize = new Sequelize(DB.NAME, DB.USER, DB.PASS, {
+  host: DB.HOST,
+  port: DB.PORT,
+  dialect: 'mysql'
+});
+
+const connection = sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err: Error) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+export default connection;
