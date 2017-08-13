4022f757e65f7a963ef999e92a2351de0f75c2f0
DovidM <landyx47@gmail.com>
2017-08-13 19:55:42 -0400

    Added  arg to issue, so can now get latest issue without knowing it's number

--- backend/src/schema/schema.ts
+++ backend/src/schema/schema.ts
@@ -24,71 +24,77 @@ import sanitize from '../helpers/sanitize';
 import db from '../db/models';
 
 const Query = new GraphQLObjectType({
-   name: 'QuerySchema',
-   description: 'Root query',
-   fields: () => ({
-       users: {
-           type: new GraphQLList(Users),
-           description: 'Users',
-           args: {
-               id: {type: GraphQLID},
-               email: {type: GraphQLString},
-           },
-           resolve: (_, args) => db.models.users.findAll({where: sanitize(args)})
-       },
-       articles: {
-           type: new GraphQLList(Articles),
-           description: 'Articles',
-           args: {
-               id: {type: GraphQLID},
-               authorid: {type: GraphQLID},
-               url: {type: GraphQLString},
-               issue: {type: GraphQLInt},
-           },
-           resolve: (_, args) => {
+    name: 'QuerySchema',
+    description: 'Root query',
+    fields: () => ({
+        users: {
+            type: new GraphQLList(Users),
+            description: 'Users',
+            args: {
+                id: {type: GraphQLID},
+                email: {type: GraphQLString},
+            },
+            resolve: (_, args) => db.models.users.findAll({where: sanitize(args)})
+        },
+        articles: {
+            type: new GraphQLList(Articles),
+            description: 'Articles',
+            args: {
+                id: {type: GraphQLID},
+                authorid: {type: GraphQLID},
+                url: {type: GraphQLString},
+                issue: {type: GraphQLInt},
+            },
+            resolve: (_, args) => {
 
-               return db.models.pageinfo.findAll({where: sanitize(args)})
-           }
-       },
-       issues: {
-           type: new GraphQLList(Issues),
-           description: 'Issues',
-           args: {
-               num: {type: GraphQLID},
-               public: {type: GraphQLBoolean}
-           },
-           resolve: (_, args: {num?: string; public?: boolean; ispublic?: number}) => {
+                return db.models.pageinfo.findAll({where: sanitize(args)})
+            }
+        },
+        issues: {
+            type: new GraphQLList(Issues),
+            description: 'Issues',
+            args: {
+                num: {type: GraphQLID},
+                public: {type: GraphQLBoolean},
+                limit: {type: GraphQLInt}
+            },
+            resolve: (_, args: {num?: string; public?: boolean; ispublic?: number, limit?: number}) => {
 
-               if ('public' in args) {
-                   args.ispublic = +args.public;
-                   delete args.public;
-               }
-               return db.models.issues.findAll({
-                   where: sanitize(args),
-                   order: [['num', 'DESC']]
-                })
-           }
-       },
-      comments: {
-           type: new GraphQLList(Comments),
-           description: 'Comments',
-           args: {
-               id: {type: GraphQLID},
-               authorid: {type: GraphQLID},
-               artId: {type: GraphQLID},
-           },
-           resolve: (_, args) => db.models.comments.findAll({where: sanitize(args)})
-       },
-      tags: {
-           type: new GraphQLList(Tags),
-           description: 'Tags of articles',
-           args: {
-               artId: {type: GraphQLID},
-           },
-           resolve: (_, args) => db.models.tags.findAll({where: sanitize(args)})
-       }
+                let limit = args.limit;
+                delete args.limit;
 
-   })
+                if ('public' in args) {
+                    args.ispublic = +args.public;
+                    delete args.public;
+                }
+
+                return db.models.issues.findAll({
+                    where: sanitize(args),
+                    order: [['num', 'DESC']],
+                    limit
+                    })
+            }
+        },
+        comments: {
+            type: new GraphQLList(Comments),
+            description: 'Comments',
+            args: {
+                id: {type: GraphQLID},
+                authorid: {type: GraphQLID},
+                artId: {type: GraphQLID},
+            },
+            resolve: (_, args) => db.models.comments.findAll({where: sanitize(args)})
+        },
+        tags: {
+            type: new GraphQLList(Tags),
+            description: 'Tags of articles',
+            args: {
+                artId: {type: GraphQLID},
+            },
+            resolve: (_, args) => db.models.tags.findAll({where: sanitize(args)})
+        }
+
+    })
 });
 
 const Mutation = new GraphQLObjectType({
