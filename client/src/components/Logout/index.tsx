d1aeda5c849e39f1cbe3c95813a36f5d861349c2
DovidM <landyx47@gmail.com>
2017-08-28 21:23:49 -0400

    Spruced up Logout component and added tests for it

--- client/src/components/Logout/index.tsx
+++ client/src/components/Logout/index.tsx
@@ -1,28 +1,15 @@
 import * as React from 'react';
-import FormContainer from '../Form/container';
 import { getJWT } from '../jwt';
 
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
 function Logout() {
 
-    const jwt = getJWT();
-
     return (
-        <Link to="/" onClick={() => jwt.level = 0}>
-            <FormContainer
-                method="put"
-                action="/api/userStatus"
-                children={<input
-                            className="changed"
-                            id="logoutInpt"
-                            type="submit"
-                            name="logout"
-                            value="Log Out"
-                            />}
-            />
+        <Link to="/">
+            <button type="button" onClick={() => window.localStorage.removeItem('jwt')}>Logout</button>
         </Link>
-    )
+    );
 }
 
 export default Logout;
---
+++ client/src/components/Logout/test.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import Logout from './';
+import { mount } from 'enzyme';
+import { MemoryRouter } from 'react-router';
+import * as renderer from 'react-test-renderer';
+
+import setFakeJwt from '../../tests/jwt.helper';
+
+setFakeJwt({level: 3});
+
+describe('<Logout>', () => {
+
+    describe('snapshots', () => {
+
+        it('renders correctly', () => {
+
+            const tree = renderer.create(
+                <MemoryRouter>
+                    <Logout/>
+                </MemoryRouter>
+            ).toJSON();
+
+            expect(tree).toMatchSnapshot();
+        });
+    });
+
+    describe('button#onClick', () => {
+
+        it('removes jwt from localstorage when clicked', () => {
+
+            const wrapper = mount(
+                <MemoryRouter>
+                    <Logout />
+                </MemoryRouter>
+            );
+
+            expect(localStorage.getItem('jwt')).toBeTruthy();
+
+            wrapper.find('button').simulate('click');
+
+            expect(localStorage.getItem('jwt')).toBeFalsy();
+        });
+    });
+});
+
