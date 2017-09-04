e8592b77d849575168d13527339780c511cc4c35
DovidM <landyx47@gmail.com>
2017-09-04 16:15:14 -0400

    Added tests

---
+++ client/src/components/Form/Label/test.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+import Label from './';
+
+describe('<Input>', () => {
+
+    describe('snapshots', () => {
+
+        /**
+         * Takes snapshots
+         */
+        function snap(abbr?: string, required: boolean = false) {
+
+            const tree = renderer.create(
+                <Label
+                    value="Label"
+                    required={required}
+                    abbr={abbr}
+                    children={<input type="text" value="hello" />}
+                />
+            ).toJSON();
+
+            expect(tree).toMatchSnapshot();
+
+        }
+
+        it('renders correctly when abbr is given and not required', () => snap('A very good hint'));
+
+        it('renders correctly when abbr is NOT given and not required', () => snap());
+
+        it('when required and no abbr', () => snap(undefined, true));
+
+        it('when required and yes abbr', () => snap('Hello', true));
+    });
+});
