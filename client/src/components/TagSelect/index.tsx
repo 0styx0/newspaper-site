c0f7ecb2d8a849baa087ba09ce6a25f0c7165868
DovidM <landyx47@gmail.com>
2017-09-05 20:54:54 -0400

    Getting tags from db

--- client/src/components/TagSelect/index.tsx
+++ client/src/components/TagSelect/index.tsx
@@ -1,30 +1,41 @@
 import * as React from 'react';
+import { TagQuery } from '../../graphql/tags';
+import { graphql, withApollo } from 'react-apollo';
 
 interface Props {
-    tags?: string[];
+    data: {
+        loading: boolean,
+        tags: [{
+            tags: string[]
+        }]
+    };
+    tags: string[];
     props: Object;
-};
+}
 
-export default function TagSelect(props: Props) {
+function TagSelect(props: Props) {
 
-    const tags = [
-                    'news',
-                    'reaction',
-                    'opinion',
-                    'poll',
-                    'features',
-                    'sports',
-                    'politics',
-                    'other'
-                ];
+    let dbTags: string[] = [];
 
-    const options = [...new Set((props.tags || []).concat(tags))].map((val =>
+    if (props.data.tags) {
+        dbTags = props.data.tags[0].tags;
+    }
+
+    const tags = [...new Set((props.tags || []).concat(dbTags))];
+
+    const options = tags.map((val =>
             <option key={val} value={val}>{val[0].toUpperCase() + val.slice(1)}</option>
         ));
 
-    const select = <select
-                     children={options}
-                   />
+    const select = (
+        <select
+            children={options}
+        />
+    );
 
     return React.cloneElement(select, props.props);
 }
+
+const TagSelectWithData = graphql(TagQuery)(TagSelect as any);
+
+export default withApollo(TagSelectWithData);
