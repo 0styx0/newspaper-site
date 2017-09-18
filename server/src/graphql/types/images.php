bad5f05081742bbeab316ff0a69f0330a1cfd416
DovidM <landyx47@gmail.com>
2017-09-17 20:58:30 -0400

    Added image type

---
+++ server/src/graphql/types/images.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once __DIR__ . '/../../../vendor/autoload.php';
+
+use Youshido\GraphQL\Execution\ResolveInfo;
+use Youshido\GraphQL\Field\AbstractField;
+use Youshido\GraphQL\Config\Field\FieldConfig;
+use Youshido\GraphQL\Type\Object\AbstractObjectType;
+use Youshido\GraphQL\Type\Scalar\StringType;
+use Youshido\GraphQL\Type\Scalar\IdType;
+use Youshido\GraphQL\Type\NonNullType;
+use Youshido\GraphQL\Type\Scalar\IntType;
+use Youshido\GraphQL\Type\Scalar\TimestampType;
+use Youshido\GraphQL\Type\Scalar\BooleanType;
+use Youshido\GraphQL\Type\ListType\ListType;
+
+class ImagesType extends AbstractObjectType {
+
+    public function build($config) {
+
+        $config->addFields([
+            'id' => new NonNullType(new IdType()),
+            'url' => new NonNullType(new StringType()),
+            'slide' => new NonNullType(new BooleanType()),
+            'artId' => new NonNullType(new IdType)
+        ]);
+    }
+}
+
+?>
