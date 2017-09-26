7889eeda4dbde9f489bb60806a283c600e9928a3
DovidM <landyx47@gmail.com>
2017-09-25 22:40:20 -0400

    Added files to composer autoload

--- server/src/test/users/helpers.php
+++ server/src/test/users/helpers.php
@@ -2,28 +2,17 @@
 
 use PHPUnit\Framework\TestCase;
 
-require_once(__DIR__ . '/../vendor/autoload.php');
+require_once(__DIR__ . '/../../../vendor/autoload.php');
 
 class UserTest extends TestCase {
 
-    protected $userExistsQuery;
-
-    function __constructor() {
-
-       $userExistsQuery = '
-                query users($profileLink: String, $id: ID) {
-                    users(profileLink: $profileLink, id: $id) {
-                    id
-                }
-            }';
-    }
 
     /**
       * @param $args - @see HelpTests::createHTTPRequest param $args
       */
     protected function request(array $args = [], $jwt = '') {
 
-        HelpTests::createHTTPRequest($args , 'users', $jwt);
+        return HelpTests::createHTTPRequest($args , 'users', $jwt);
     }
 
 }
