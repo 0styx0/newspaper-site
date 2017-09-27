c317cb2b87499ea40bd90f3a53705aee5bb02c5c
DovidM <landyx47@gmail.com>
2017-09-27 17:53:18 -0400

    Added tests

---
+++ server/src/test/users/queries/UserLoggedInTest.php
@@ -0,0 +1,121 @@
+<?php
+
+
+require_once(__DIR__ . '/../../../../vendor/autoload.php');
+require_once(__DIR__ . '/../helpers.php');
+
+class UserLoggedInTest extends UserTest {
+
+    function testCanSeeFullArticleCount() { // both public and private
+
+        $user = $this->TestDatabase->getRandomUser();
+
+        $expected = count($this->TestDatabase->pageinfo);
+
+        $data = $this->request([
+           'query' => 'query users {
+                           users {
+                               articleCount
+                           }
+                       }'
+        ], HelpTests::getJwt($user));
+
+        $this->assertEqual($expected, $data['users'][0]['articleCount']);
+    }
+
+    function testCanSeeAnyArticle() {
+
+        $user = $this->TestDatabase->getRandomUser();
+        $expectedIds = array_column($this->TestDatabase->pageinfo, 'id');
+
+        $data = $this->request([
+            'query' => 'query users {
+                            users {
+                                articles {
+                                    id
+                                }
+                            }
+                        }'
+        ], HelpTests::getJwt($user));
+
+        $this->expectEqual($expectedIds, array_column($data['users'][0]['articles'], 'id'));
+    }
+
+    function testLevelHigherThanOtherUserCanEdit() {
+
+        $higherLevel = rand(2, 3);
+
+        $user = HelpTests::searchArray($this->TestDatabase->users, function (array $currentUser) {
+            return $currentUser['level'] == $higherLevel;
+        });
+
+        $data = $this->request([
+            'query' => 'query users {
+                            users {
+                                canEdit
+                            }
+                        }'
+        ], HelpTests::getJwt($user));
+
+        $lowerLevelUsers = HelpTests::searchArray($data['users'], function (array $currentUser) {
+            return $currentUser['level'] < $higherLevel;
+        });
+
+        $this->assertFalse(in_array(false, array_column($lowerLevelUsers, 'canEdit')));
+    }
+
+    function testLevelSameOrLowerThanOtherUserCannotEdit() {
+
+        $level = rand(1, 3);
+
+        $user = HelpTests::searchArray($this->TestDatabase->users, function (array $currentUser) {
+            return $currentUser['level'] == $level;
+        });
+
+        $data = $this->request([
+            'query' => 'query users {
+                            users {
+                                canEdit
+                            }
+                        }'
+        ], HelpTests::getJwt($user));
+
+        $users = HelpTests::searchArray($data['users'], function (array $currentUser) {
+            return $currentUser['level'] >= $higherLevel && $user['id'];
+        });
+
+        $this->assertFalse(in_array(true, array_column($lowerLevelUsers, 'canEdit')));
+    }
+
+    function testCanSeeOwnSettings() {
+
+        $user = $this->getRandomUser();
+
+        $data = $this->request([
+            'query' => 'query users($id: ID) {
+                            users(id: $id) {
+                                notifications
+                                twoFactor
+                                canEdit
+                            }
+                        }',
+            'variables' => [
+                'id' => $user['id']
+            ]
+        ]);
+
+        $actualUser = $data['users'][0];
+
+        $this->assertTrue($actualUser['canEdit']);
+        $this->assertEqual($user['notifications'], $actualUser['notifications']);
+        $this->assertEqual($user['two_fa_enabled'], $actualUser['twoFactor']);
+    }
+}
+
+
+
+
+
+
+
+?>
