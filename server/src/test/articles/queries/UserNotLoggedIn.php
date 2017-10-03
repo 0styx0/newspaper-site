40b0a18b2122094df817b09e410e912ff862ddf9
DovidM <landyx47@gmail.com>
2017-10-03 07:33:14 -0400

    Added tests

--- server/src/test/articles/queries/UserNotLoggedIn.php
+++ server/src/test/articles/queries/UserNotLoggedIn.php
@@ -7,10 +7,42 @@ class UserNotLoggedInTest extends ArticleTest {
 
     function testCannotQueryArticlesThatArePrivate() {
 
+        $article = HelpTests::searchArray($this->Database->GenerateRows->pageinfo, function (array $currentArticle) {
+            return $currentArticle['issue'] == $this->Database->GenerateRows->issues[0]['num'];
+        });
+
+        $data = $this->request([
+            'query' => 'query articles($id: ID) {
+                            articles(id: $id) {
+                                id
+                            }
+                        }',
+            'variables' => [
+                'id' => $article['id']
+            ]
+        ]);
+
+        $this->assertNull($data['articles'][0]);
     }
 
     function testCannotEditAnyArticles() {
 
+        $article = HelpTests::searchArray($this->Database->GenerateRows->pageinfo, function (array $currentArticle) {
+            return $currentArticle['issue'] != $this->Database->GenerateRows->issues[0]['num']; // so get a public article
+        });
+
+        $data = $this->request([
+            'query' => 'query articles($id: ID) {
+                            articles(id: $id) {
+                                canEdit
+                            }
+                        }',
+            'variables' => [
+                'id' => $article['id']
+            ]
+        ]);
+
+        $this->assertFalse($data['articles'][0]['canEdit']);
     }
 }
 
