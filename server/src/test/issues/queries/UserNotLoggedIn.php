fa13a0a36b94633f72346f7a8c32b2f3823bac28
DovidM <landyx47@gmail.com>
2017-10-09 14:52:48 -0400

    Added tests

--- server/src/test/issues/queries/UserNotLoggedIn.php
+++ server/src/test/issues/queries/UserNotLoggedIn.php
@@ -7,9 +7,46 @@ class NotLoggedInIssueTest extends IssueTest {
 
     function testCanOnlyGetPublicIssues() {
 
+        $data = $this->request([
+            'query' => "query IssueInfo {
+                            issues {
+                                num
+                                name
+                            }
+                        }"
+        ]);
+
+        $expectedCount = 0;
+
+        foreach ($this->Database->GenerateMockRows->issues as $issue) {
+
+            if ($issue['public']) {
+                $expectedCount++;
+            }
+        }
+
+        $this->assertEqual($expectedCount, count($data['issues']));
     }
 
     function testGetMostRecentPublicIssueIfNoArgPassed() {
 
+        $data = $this->request([
+            'query' => 'query IssueInfo($num: ID) {
+                            issues(num: $num, limit: 1) {
+                                num
+                                max
+                                name
+                            }
+                        }',
+            'variables' => [
+                'num' => $this->Database->GenerateMockRows->issues[0]['num'] + 1
+            ]
+        ]);
+
+        $publicIssue = HelpTests::searchArray($this->Database->GenerateMockRows->issues, function ($issue) {
+            return $issue['public'];
+        });
+
+        $this->assertEqual($publicIssue['num'], $data['issues'][0]['num']);
     }
 }
