9f54fc33aca462a5d9cbaa8fba50f4c7835802fe
DovidM <landyx47@gmail.com>
2017-10-09 15:01:24 -0400

    Added test skeleton

---
+++ server/src/test/issues/mutations/updateIssue.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once(__DIR__ . '/../../../../vendor/autoload.php');
+require_once(__DIR__ . '/../helpers.php');
+
+class UpdateIssueTest extends IssueTest {
+
+    function testCannotModifyIfNotLoggedIn() {
+
+    }
+
+    function testCannotModifyIfBadPassword() {
+
+    }
+
+    function testCannotModifyIfLevelLessThanThree() {
+
+    }
+
+    function testCanModifyName() {
+
+    }
+
+    function testCanMakeIssuePublic() {
+
+    }
+
+    function testCannotMakeIssuePrivate() {
+
+    }
+}
+?>
