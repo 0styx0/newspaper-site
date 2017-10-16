e37c1f35899da41cec9c6f895c4db1bc06a08413
DovidM <landyx47@gmail.com>
2017-10-16 18:20:18 -0400

    Added tests

--- server/src/test/allTags/query/mutations/createTagTest.php
+++ server/src/test/allTags/query/mutations/createTagTest.php
@@ -24,17 +24,48 @@ class CreateTagTest extends AllTagsTest {
             ]
         ], HelpTests::getJwt($user));
     }
-    
+
     function testNotLevelThreeCannotAddTag() {
 
+        $user = HelpTests::searchArray($this->Database->GenerateRows->users, function (array $currentUser) {
+            return $currentUser['level'] < 3;
+        });
+
+        $tag = HelpTests::faker()->randomWord();
+
+        $data = $this->helpTest($tag, $user);
+
+        $this->assertNull($data['tag']);
     }
 
     function testGoodLevelThreeCanAddTag() {
 
+        $user = HelpTests::searchArray($this->Database->GenerateRows->users, function (array $currentUser) {
+            return $currentUser['level'] > 2;
+        });
+
+        $tag = HelpTests::faker()->randomWord();
+
+        $data = $this->helpTest($tag, $user);
+
+        $this->assertEqual($tag, $data['tag']);
     }
 
     function testMaliciousDataNotAccepted() {
 
+        $user = HelpTests::searchArray($this->Database->GenerateRows->users, function (array $currentUser) {
+            return $currentUser['level'] > 2;
+        });
+
+        foreach (HelpTests::unsafeData as $tag) {
+
+            $data = $this->helpTest($tag, $user);
+
+            $this->assertNull($data['tag']);
+            $dbTag = Db::query("SELECT tag FROM tag_list WHERE tag = ?", [$tag])->fetchColumn();
+
+            $this->assertNull($dbTag);
+        }
     }
 }
 ?>
