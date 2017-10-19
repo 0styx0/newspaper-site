16617594b4d124fce9d6f7984bd48b3fad75ef2b
DovidM <landyx47@gmail.com>
2017-10-19 12:28:28 -0400

    Switched Errors to Exceptions

--- server/src/helpers/Guard.php
+++ server/src/helpers/Guard.php
@@ -7,14 +7,34 @@ class Guard {
     /**
      * Makes sure user is logged in
      *
-     * @throw Error 'User must be logged in'
+     * @throws Exception 'User must be logged in'
      *
      * @return true if user is logged in, error otherwise
      */
     public static function userMustBeLoggedIn() {
 
         if (!Jwt::getToken()) {
-            throw new Error('User not logged in');
+            throw new Exception('User not logged in');
+        }
+
+        return true;
+    }
+
+    /**
+     * Makes sure user is logged in and a certain level
+     *
+     * @param $level - lowest level user can be
+     *
+     * @throws 'User must be logged in'
+     * @throws 'User must be at least level $level'
+     *
+     */
+    public static function userMustBeLevel(int $level) {
+
+        Guard::userMustBeLoggedIn();
+
+        if (Jwt::getToken()->getClaim('level') < $level) {
+            throw new Exception("User must be at least level {$level}");
         }
 
         return true;
