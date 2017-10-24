c71981a18435e5848d2ca381c3857c4a36511081
DovidM <landyx47@gmail.com>
2017-10-24 10:08:14 -0400

    Fixed: Unable to modify jwt after initial creation

--- server/src/helpers/Jwt.php
+++ server/src/helpers/Jwt.php
@@ -1,6 +1,9 @@
 <?php
 
+require_once __DIR__ . '/../../vendor/autoload.php';
 use Lcobucci\JWT\Parser;
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Signer\Hmac\Sha256;
 
 /**
 * Singleton to get json web token
@@ -45,14 +48,14 @@ class Jwt {
      */
     public static function setToken(array $user) {
 
+        $signer = new Sha256();
+
         $token = (new Builder())->setIssuer('https://tabceots.com')
                                 ->setAudience('https://tabceots.com')
                                 ->setIssuedAt(time())
                                 ->setExpiration(time() + 3600)
                                 ->setId($user['id'], true)
-                                ->set('id', $user['id'])
-                                ->sign($signer, $_ENV['JWT_SECRET'])
-                                ->getToken(); // Retrieves the generated token
+                                ->set('id', $user['id']);
 
         $emailIsVerified = $user['profileLink'][0] !== '.';
 
@@ -62,7 +65,7 @@ class Jwt {
                             ->set('level', $user['level']);
         }
 
-        return $token;
+        return $token->sign($signer, $_ENV['JWT_SECRET'])->getToken(); // Retrieves the generated token
     }
 
 }
