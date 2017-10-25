434cb80572af666d2da896c07d03a1b50467c123
DovidM <landyx47@gmail.com>
2017-10-25 07:25:12 -0400

    Fixed: Stopped disruption of field uniqueness

--- server/src/test/GenerateMockRow.php
+++ server/src/test/GenerateMockRow.php
@@ -13,86 +13,94 @@ use Faker\Provider\Base;
   */
 class GenerateMockRow {
 
-    public function user(Faker\Generator $faker) {
+    private static $faker; // as to avoid duplicate fields later, need 1 instance of faker only
+
+    public function __construct() {
+        GenerateMockRow::$faker = Faker\Factory::create();
+    }
+
+    public function user() {
 
         return [
-            'id' => $faker->unique()->randomNumber() + 1,
-            'username' => $faker->unique()->userName(),
-            'f_name' => $faker->firstName(),
-            'm_name' => $faker->boolean() ? $faker->suffix() : null,
-            'l_name' => $faker->lastName(),
-            'password' => $faker->password(),
-            'email' => $faker->email(),
+            'id' => GenerateMockRow::$faker->unique()->randomNumber() + 1,
+            'username' => GenerateMockRow::$faker->unique()->userName(),
+            'f_name' => GenerateMockRow::$faker->firstName(),
+            'm_name' => GenerateMockRow::$faker->boolean() ? GenerateMockRow::$faker->suffix() : null,
+            'l_name' => GenerateMockRow::$faker->lastName(),
+            'password' => GenerateMockRow::$faker->password(),
+            'email' => GenerateMockRow::$faker->email(),
             'level' => rand(1, 3),
-            'auth' => $faker->password(),
-            'auth_time' => $faker->date(),
-            'notifications' => +$faker->boolean(),
-            'two_fa_enabled' => +$faker->boolean()
+            'auth' => GenerateMockRow::$faker->password(),
+            'auth_time' => GenerateMockRow::$faker->date(),
+            'notifications' => +GenerateMockRow::$faker->boolean(),
+            'two_fa_enabled' => +GenerateMockRow::$faker->boolean()
         ];
     }
 
-    public function tag_list(Faker\Generator $faker) {
+    public function tag_list() {
 
         return [
-          'tag' => $faker->unique()->word().substr(0, 20) // varchar(20)
+          'tag' => GenerateMockRow::$faker->unique()->word().substr(0, 20) // varchar(20)
         ];
     }
 
-    public function issue(Faker\Generator $faker) {
+    public function issue() {
 
         return [
-            'num' => $faker->unique()->randomNumber(),
+            'num' => GenerateMockRow::$faker->unique()->randomNumber(),
             'ispublic' => 1,
-            'name' => $faker->unique()->name(),
-            'madepub' => $faker->date()
+            'name' => GenerateMockRow::$faker->unique()->name(),
+            'madepub' => GenerateMockRow::$faker->date()
         ];
     }
 
-    public function pageinfo(Faker\Generator $faker) {
+    public function pageinfo() {
+
+        $faker = GenerateMockRow::$faker;
 
         return [
-            'id' => $faker->unique()->randomNumber() + 1,
-            'created' => $faker->date(),
-            'url' => $faker->unique()->domainWord(),
+            'id' => GenerateMockRow::$faker->unique()->randomNumber() + 1,
+            'created' => GenerateMockRow::$faker->date(),
+            'url' => GenerateMockRow::$faker->unique()->domainWord(),
             'lede' => "
                 <h1>{$faker->word()}</h1>
                 <h4>{$faker->name()}</h4>
                 <p>{$faker->paragraph()}</p>",
-            'body' => $faker->randomHtml(),
-            'issue' => $faker->randomNumber(), // todo: replace, foreign key
-            'authorid' => $faker->randomNumber(), // ditto
-            'views' => $faker->randomNumber(),
-            'display_order' => $faker->randomNumber()
+            'body' => GenerateMockRow::$faker->randomHtml(),
+            'issue' => GenerateMockRow::$faker->randomNumber(), // todo: replace, foreign key
+            'authorid' => GenerateMockRow::$faker->randomNumber(), // ditto
+            'views' => GenerateMockRow::$faker->randomNumber(),
+            'display_order' => GenerateMockRow::$faker->randomNumber()
         ];
     }
 
-    public function image(Faker\Generator $faker) {
+    public function image() {
 
         return [
-            'id' => $faker->unique()->randomNumber() + 1,
-            'art_id' => $faker->unique()->randomNumber(),
-            'slide' => +$faker->boolean(),
-            'url' => $faker->imageUrl()
+            'id' => GenerateMockRow::$faker->unique()->randomNumber() + 1,
+            'art_id' => GenerateMockRow::$faker->unique()->randomNumber(),
+            'slide' => +GenerateMockRow::$faker->boolean(),
+            'url' => GenerateMockRow::$faker->imageUrl()
         ];
     }
 
-    public function tag(Faker\Generator $faker) {
+    public function tag() {
 
         return [
-            'id' => $faker->unique()->randomNumber() + 1,
-            'tag' => $faker->word(), // to be replaced
-            'art_id' => $faker->unique()->randomNumber(), // ditto
+            'id' => GenerateMockRow::$faker->unique()->randomNumber() + 1,
+            'tag' => GenerateMockRow::$faker->word(), // to be replaced
+            'art_id' => GenerateMockRow::$faker->unique()->randomNumber(), // ditto
         ];
     }
 
-    public function comment(Faker\Generator $faker) {
+    public function comment() {
 
         return [
-            'id' => $faker->unique()->randomNumber() + 1,
+            'id' => GenerateMockRow::$faker->unique()->randomNumber() + 1,
             'art_id' => -1, // replaced later
             'authorid' => -1, // replaced later
-            'content' => $faker->randomHtml(),
-            'created' => $faker->date()
+            'content' => GenerateMockRow::$faker->randomHtml(),
+            'created' => GenerateMockRow::$faker->date()
         ];
     }
 }
