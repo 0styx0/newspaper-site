a958bb4614b5d0e325d66d696ec5be2e6c57e711
DovidM <landyx47@gmail.com>
2017-11-02 16:20:33 -0400

    Added file

--- server/src/test/runAll.php
+++ server/src/test/runAll.php
@@ -3,11 +3,12 @@
 $filesToTest = [
     'articles/mutations/createArticleTest.php',
     'articles/mutations/deleteArticleTest.php',
-    'articles/mutations/updateArticleTest.php'
+    'articles/mutations/updateArticleTest.php',
+    'articles/queries/UserLoggedInTest.php'
 ];
 
 echo `clear`;
 
 foreach ($filesToTest as $file) {
-    echo `../../vendor/phpunit/phpunit/phpunit {$file} --stop-on-failure --stop-on-error`;
+    echo `../../vendor/phpunit/phpunit/phpunit {$file} --stop-on-failure --stop-on-error --stderr`;
 }
