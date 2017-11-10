2166500b71cfcfeaab72449e5d332782a3705980
DovidM <landyx47@gmail.com>
2017-11-10 13:16:05 -0500

    Moved file

--- server/src/test/login/helpers.php
+++ server/src/test/login/helpers.php
@@ -4,7 +4,7 @@ use PHPUnit\Framework\TestCase;
 
 require_once(__DIR__ . '/../../../vendor/autoload.php');
 
-class LoginTest extends TestCase {
+class LoginHelperTest extends HelpTests {
 
     protected $TestDatabase;
 
