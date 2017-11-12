e78fbd726ab4c7c3af67ae22206dbc45d5869230
DovidM <landyx47@gmail.com>
2017-11-12 13:56:09 -0500

    Extend Helptests

--- server/src/test/tags/helpers.php
+++ server/src/test/tags/helpers.php
@@ -4,7 +4,7 @@ use PHPUnit\Framework\TestCase;
 
 require_once(__DIR__ . '/../../../vendor/autoload.php');
 
-class AllTagsTest extends TestCase {
+class AllTagsTest extends HelpTests {
 
     protected $Database;
 
