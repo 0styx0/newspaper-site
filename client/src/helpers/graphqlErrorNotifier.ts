248d89d247d5eb978965cf409a8b90aae03e799a
DovidM <landyx47@gmail.com>
2017-11-26 16:31:07 -0500

    Can notify on success

--- client/src/helpers/graphqlErrorNotifier.ts
+++ client/src/helpers/graphqlErrorNotifier.ts
@@ -1,10 +1,18 @@
 import Notification from './Notification/Notification';
 
-export default async function (query: Function, params: Object): Promise<any> {
+export default async function (query: Function, params: Object, successMessage?: string): Promise<any> {
 
     return await query(params)
+        .then(() => {
+
+            if (successMessage) {
+                Notification({
+                    body: successMessage
+                });
+            }
+        })
         .catch((e: Error) => {
-            
+
             Notification({
                 body: e.message.split(': ')[1]
             });
