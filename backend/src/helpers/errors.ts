82a037e87739963a27bb9f0f5ad77e951ea4098b
DovidM <landyx47@gmail.com>
2017-12-08 09:58:19 -0500

    server/ is replacing backend/

--- backend/src/helpers/errors.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    password: 'Incorrect Password',
-    noUser: 'User Not Logged In',
-    authCode: 'Incorrect Auth Code',
-    authority: 'Insufficient Authority',
-    unknown: 'Unknown Error'
-}
